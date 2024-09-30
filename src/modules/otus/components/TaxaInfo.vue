e30c4dc802711a755879f6badc5a4957a4789639
W Beecher Baker <wbbaker@purdue.edu>
2024-09-30 11:24:34 -0400

    handle undefined taxon

--- src/modules/otus/components/TaxaInfo.vue
+++ src/modules/otus/components/TaxaInfo.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <h2 class="text-1xl capitalize">
-      {{ store.taxon.rank || 'Combination' }}
+      {{ store.taxon?.rank || 'Combination' }}
     </h2>
     <h1 class="text-xl dark:text-gray-100">
       <span>
         <span
-          :title="store.taxon.short_status"
-          v-html="store.taxon.full_name_tag"
+          :title="store.taxon?.short_status"
+          v-html="store.taxon?.full_name_tag"
         />
         <span
-          v-if="!store.taxon.is_valid"
+          v-if="!store.taxon?.is_valid"
           class="ml-1 text-red-600"
           title="Invalid"
         >
