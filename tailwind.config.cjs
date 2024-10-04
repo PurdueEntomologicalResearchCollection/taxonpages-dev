b2afb307666b212dbcffa43dda911191672bf862
W Beecher Baker <wbbaker@purdue.edu>
2024-10-04 15:06:02 -0400

    switch main font to acumin pro

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -25,7 +25,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        main: ['United Sans', 'sans-serif']
+        main: ["acumin-pro", "sans-serif"],
       },
       colors: {
         base: {
